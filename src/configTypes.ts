0be03ce052455bc1f4443922c636e8be7b16e572
Elias Liedholm <github@eliasliedholm.com>
2021-05-13 11:35:03 +0200

    update device config types

--- src/configTypes.ts
+++ src/configTypes.ts
@@ -12,6 +12,12 @@ export type DeviceConfig = {
   name?: string;
   on_code?: number;
   off_code?: number;
+  color_codes?: Array<number>;
+  temperature_color_codes?: Array<number>;
+  brigthness_up_code: number;
+  brightness_down_code: number;
+  brigthness_steps: number;
+  color_white_code: number;
   pulseLength?: number;
   protocol?: number;
   codelength?: number;
